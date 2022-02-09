eedd52bbfa3568ca536ecc1e814c2009391fc581
Aleksandr Volodin <cursedkun@gmail.com>
2022-02-09 21:38:35 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,18 +9,24 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import java.util.Locale;
 
 public class SqlRuParse {
+    private static final int MAX_PAGES = 5;
+
     public static void main(String[] args) throws Exception {
 
         Locale.setDefault(Locale.ENGLISH);
+        int currentPage = 1;
 
         SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select("td[style].altCol");
-        for (Element td : row) {
-            Element parent = td.parent();
-            System.out.println(parent.child(1).getElementsByIndexEquals(0).attr("href"));
-            System.out.println(parent.child(1).getElementsByIndexEquals(0).text());
-            System.out.println(parser.parse(parent.child(5).text()));
+        while (currentPage <= MAX_PAGES) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + currentPage).get();
+            Elements row = doc.select("td[style].altCol");
+            for (Element td : row) {
+                Element parent = td.parent();
+                System.out.println(parent.child(1).getElementsByIndexEquals(0).attr("href"));
+                System.out.println(parent.child(1).getElementsByIndexEquals(0).text());
+                System.out.println(parser.parse(parent.child(5).text()));
+            }
+            currentPage++;
         }
     }
 }
